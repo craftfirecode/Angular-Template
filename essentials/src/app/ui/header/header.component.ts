4096a4f19ce04dca8cd631e4100855030a4f7af5
Jokoson <exampleexampleexample12135example@example.com>
2025-05-26 09:31:08 +0200

    add logo image to header component

--- essentials/src/app/ui/header/header.component.ts
+++ essentials/src/app/ui/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  get imagePath(): string {
+    return 'assets/brand/';
+  }
 }
