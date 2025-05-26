a5a76dbca2013286ba1a267cb139efb32e3a475f
Jokoson <exampleexampleexample12135example@example.com>
2025-05-26 11:07:21 +0200

    refactor user component by removing unused imports and simplifying the template

--- essentials/src/app/ui/user/user.component.ts
+++ essentials/src/app/ui/user/user.component.ts
@@ -1,18 +1,14 @@
-import {Component, computed, input, signal,} from '@angular/core';
+import {Component, input} from '@angular/core';
 import {IUsers} from './userDump';
-import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-user',
-  imports: [
-    NgIf
-  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
   data = input.required<IUsers>();
-  
+
   onClickUser(): void {
     console.log(JSON.stringify(this.data));
   }
