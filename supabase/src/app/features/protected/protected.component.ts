f7aa1d0aaf19d94ebc5a617087ca4a6e05c1f073
jokosonso <und@und.djaios>
2025-08-28 09:51:43 +0200

    feat: add trash icon button to folder list and update alignment styles in ProtectedComponent

--- supabase/src/app/features/protected/protected.component.ts
+++ supabase/src/app/features/protected/protected.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {SupabaseService} from '../../core';
 import {supabaseRealtimeFolders} from '../../supabaseRealtimeFolders';
 import {MatButton} from '@angular/material/button';
+import {LucideAngularModule, FileIcon} from 'lucide-angular';
 
 @Component({
   selector: 'app-protected',
   standalone: true,
-  imports: [CommonModule, MatButton],
+  imports: [CommonModule, MatButton, LucideAngularModule],
   template: `
     <main>
       <h2>Protected Area</h2>
@@ -16,8 +17,19 @@ import {MatButton} from '@angular/material/button';
 
       <div class="flex-row">
         @for (folder of data.folderList(); track folder.id) {
-          <div class="my-5">
-            <a class="w-full" matButton="outlined" [href]="'protected/todos/' + folder.id">{{ folder.id }} – {{ folder.title }}</a>
+          <div class="my-5 flex gap-5 items-center">
+            <a class="w-full left" matButton="outlined" [href]="'protected/todos/' + folder.id">
+              {{ folder.id }} – {{ folder.title }}
+            </a>
+            <button matButton="outlined">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                   stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                   class="lucide lucide-trash-icon lucide-trash">
+                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/>
+                <path d="M3 6h18"/>
+                <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
+              </svg>
+            </button>
           </div>
         }
       </div>
@@ -25,7 +37,10 @@ import {MatButton} from '@angular/material/button';
   `
 })
 export class ProtectedComponent {
-  constructor(public auth: SupabaseService, public data: supabaseRealtimeFolders) {}
+  constructor(public auth: SupabaseService, public data: supabaseRealtimeFolders) {
+  }
+
+  readonly FileIcon = FileIcon;
 
   logout() {
     this.auth.signOut();
