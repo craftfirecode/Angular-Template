f5b2afbb74ba0e5c62f71e9ab6c12c2f39094ea6
jokosonso <und@und.djaios>
2025-08-28 11:53:45 +0200

    refactor: move todos page template to separate HTML file

--- supabase/src/app/features/protected/todos/todos-page.component.ts
+++ supabase/src/app/features/protected/todos/todos-page.component.ts
@@ -13,35 +13,7 @@ import {MatButton, MatFabButton} from '@angular/material/button';
   selector: 'app-todos-page',
   standalone: true,
   imports: [CommonModule, MatBottomSheetModule, MatButton, MatFabButton],
-  template: `
-    <main class="mt-5 pb-30">
-      <a href="/supabase/public" matButton="outlined">Zurück</a>
-      <div class="my-5">
-        <div *ngFor="let todo of filteredTodos()">
-          <div class="my-5 flex gap-5 items-center">
-            <button class="w-full left" matButton="outlined">{{ todo.title }}</button>
-            <button matButton="outlined" (click)="deleteTodo(todo.id)">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                   stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                   class="lucide lucide-trash-icon lucide-trash">
-                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/>
-                <path d="M3 6h18"/>
-                <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
-      <button matFab="elevated" (click)="openBottomSheet()" class="fab">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-             class="lucide lucide-plus-icon lucide-plus">
-          <path d="M5 12h14"/>
-          <path d="M12 5v14"/>
-        </svg>
-      </button>
-    </main>
-  `
+  templateUrl: './todo.html',
 })
 export class TodosPageComponent {
   private folderService = inject(FolderService);
