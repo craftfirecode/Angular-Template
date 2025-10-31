ac3931418ee60ab803a981201d6903981ea452c3
jokosonso <und@und.djaios>
2025-10-31 11:16:08 +0100

    chore: upgrade Angular dependencies to version 20.3.9 and TypeScript to 5.9.3
    refactor(api.service.ts): use inject() for HttpClient instantiation
    
    refactor(users.component.html): update template syntax to use new structural directives
    
    refactor(users.component.ts): use inject() for ApiService and clean up imports

--- ng/src/app/api.service.ts
+++ ng/src/app/api.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 export interface User {
   id: number;
@@ -13,7 +13,7 @@ export interface User {
 })
 
 export class ApiService {
-  constructor(private http: HttpClient) {}
+  private http = inject(HttpClient);
 
   getUsers(): Observable<User[]> {
     return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users');
--- ng/src/app/pages/users/users.component.ts
+++ ng/src/app/pages/users/users.component.ts
@@ -1,21 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AsyncPipe, NgForOf } from '@angular/common';
 import { ApiService, User } from '../../api.service';
-import {Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-users',
-  imports: [
-    NgForOf,
-    AsyncPipe
-  ],
+  imports: [AsyncPipe],
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
+
 export class UsersComponent implements OnInit {
   users$!: Observable<User[]>;
 
-  constructor(private apiService: ApiService) {}
+  private apiService = inject(ApiService);
 
   ngOnInit() {
     this.users$ = this.apiService.getUsers();
