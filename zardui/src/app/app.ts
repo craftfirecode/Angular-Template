0d4f49ca97f808e15476998c00b87fa22df590c7
jokosonso <und@und.djaios>
2025-11-01 12:27:12 +0100

    add accordion

--- zardui/src/app/app.ts
+++ zardui/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
+import { ZardAccordionItemComponent } from '@shared/components/accordion/accordion-item.component';
+import { ZardAccordionComponent } from '@shared/components/accordion/accordion.component';
 import { ZardButtonComponent } from '@shared/components/button/button.component';
 import { ZardIconComponent } from '@shared/components/icon/icon.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [ZardButtonComponent, ZardIconComponent],
+  imports: [ZardButtonComponent, ZardIconComponent, ZardAccordionComponent, ZardAccordionItemComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
