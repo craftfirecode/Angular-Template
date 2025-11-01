c91240409c9c1bbf3ec8744a06192d9266a9464a
jokosonso <und@und.djaios>
2025-11-01 15:28:09 +0100

    feat(ui): enhance button components and add new icons in app

--- spartan/src/app/app.ts
+++ spartan/src/app/app.ts
@@ -1,14 +1,16 @@
 import { Component, signal } from '@angular/core';
 
-import { NgIcon } from '@ng-icons/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
 import { HlmAccordionImports } from '@spartan-ng/helm/accordion';
 import { HlmIconImports } from '@spartan-ng/helm/icon';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
-
+import { HlmIcon } from '@spartan-ng/helm/icon';
+import { lucideChevronRight } from '@ng-icons/lucide';
 @Component({
   selector: 'app-root',
-  imports: [NgIcon, HlmIconImports, HlmAccordionImports, HlmButtonImports ],
+  imports: [NgIcon, HlmIcon, HlmIconImports, HlmAccordionImports, HlmButtonImports],
   templateUrl: './app.html',
+  providers: [provideIcons({ lucideChevronRight })],
   styleUrl: './app.scss'
 })
 export class App {
