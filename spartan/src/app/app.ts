0469a85d5726609b7ddc322843f062dcb7cf612c
jokosonso <und@und.djaios>
2025-11-02 10:13:39 +0100

    feat(date-picker): add HlmDatePicker and HlmDateRangePicker components with configuration tokens
    feat(form-field): introduce HlmFormField, HlmError, HlmHint components for form validation
    
    feat(popover): implement HlmPopoverContent and HlmPopoverClose directives for popover functionality
    
    feat(select): create HlmSelect and related components for enhanced select functionality
    
    fix(app): integrate HlmDatePicker into the main application with date range configuration
    
    chore(tsconfig): update paths for new date-picker, popover, form-field, and select libraries

--- spartan/src/app/app.ts
+++ spartan/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, model, signal } from '@angular/core';
 
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { HlmAccordionImports } from '@spartan-ng/helm/accordion';
@@ -6,13 +6,25 @@ import { HlmIconImports } from '@spartan-ng/helm/icon';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
 import { HlmIcon } from '@spartan-ng/helm/icon';
 import { lucideChevronRight } from '@ng-icons/lucide';
+import { HlmDatePickerImports } from '@spartan-ng/helm/date-picker';
+
 @Component({
   selector: 'app-root',
-  imports: [NgIcon, HlmIcon, HlmIconImports, HlmAccordionImports, HlmButtonImports],
+  imports: [NgIcon, HlmIcon, HlmIconImports, HlmAccordionImports, HlmButtonImports, HlmDatePickerImports],
   templateUrl: './app.html',
   providers: [provideIcons({ lucideChevronRight })],
   styleUrl: './app.scss'
 })
+
 export class App {
   protected readonly title = signal('spartan');
+
+  /** The minimum date */
+  public minDate = new Date(2023, 0, 1);
+
+  /** The maximum date */
+  public maxDate = new Date(2035, 11, 31);
+
+  protected readonly _captionLayout = model<'dropdown' | 'label' | 'dropdown-months' | 'dropdown-years'>('dropdown');
+
 }
